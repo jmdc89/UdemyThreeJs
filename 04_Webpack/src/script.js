44d95b211dd7f694d375972a702729d1aed2ec6a
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-08-29 10:33:44 +0300

    07_Camera
    starter pack

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -1,18 +1,11 @@
 import './style.css'
 import * as THREE from 'three'
-import gsap from 'gsap'
 
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 // Sizes
 const sizes = {
@@ -20,9 +13,22 @@ const sizes = {
     height: 600
 }
 
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
 scene.add(camera)
 
 // Renderer
@@ -31,37 +37,21 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-// Animation
+// Animate
+const clock = new THREE.Clock()
 
-//let time = Date.now()
-
-//const clock = new THREE.Clock()
-
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
-gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
-
-const tick = () => {
-
-    // Time
-    //const currentTime = Date.now()
-    //const deltaTime = currentTime - time
-    //time = currentTime
-    //const elapsedTime = clock.getElapsedTime()
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    //mesh.rotation.y += 0.01 * deltaTime
-    //mesh.rotation.y = elapsedTime
-    //mesh.rotation.x = elapsedTime
-    //mesh.position.x = Math.cos(elapsedTime)
-    //mesh.position.y = Math.sin(elapsedTime)
-    //camera.lookAt(mesh.position)
+    mesh.rotation.y = elapsedTime;
 
     // Render
     renderer.render(scene, camera)
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
-
 }
 
 tick()
