12d9d0d80b12f656f163865e02e330e48e2a49e0
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-11 16:24:19 +0300

    Update script.js
    textureLoader

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -58,9 +58,24 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Textures
+ */
+ const textureLoader = new THREE.TextureLoader()
+
+ const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+ const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+ const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+ const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+ const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+ const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+ const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+ const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+ const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+
+/**
  * Objects
  */
- const material = new THREE.MeshBasicMaterial()
+ const material = new THREE.MeshBasicMaterial({ map: doorColorTexture})
 
  const sphere = new THREE.Mesh(
      new THREE.SphereGeometry(0.5, 16, 16),
