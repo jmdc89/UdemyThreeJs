88c305bd97a772e3570aaabed6dd486128b60019
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-12 21:29:04 +0300

    Update script.js
    material.opacity

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -84,7 +84,10 @@ material.color = new THREE.Color('red')
 material.color = new THREE.Color('rgb(255, 0, 0)')
 material.color = new THREE.Color(0xff0000)
 
-material.wireframe = true
+// material.wireframe = true
+
+material.transparent = true
+material.opacity = 0.5
 
  const sphere = new THREE.Mesh(
      new THREE.SphereGeometry(0.5, 16, 16),
