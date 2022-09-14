a1bf4cde90427ea4e8e6f8f4af1d4173981bafbe
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-14 15:12:06 +0300

    Update script.js
    textGeometry

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 /**
  * Base
@@ -24,12 +25,12 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
@@ -48,7 +49,23 @@ const sizes = {
      '/fonts/helvetiker_regular.typeface.json',
      (font) =>
      {
-         console.log('loaded')
+         const textGeometry = new TextGeometry(
+            'Hello Three.js',
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+         )
+         const textMaterial = new THREE.MeshBasicMaterial({wireframe:true})
+         const text = new THREE.Mesh(textGeometry, textMaterial)
+         scene.add(text)
      }
  )
 
