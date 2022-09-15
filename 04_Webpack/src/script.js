cc03d55ff4e9a07b3514650b744c9b442b23f3dc
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-15 10:13:39 +0300

    Update script.js
    Hemisphere light

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -27,6 +27,9 @@ const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3)
 directionalLight.position.set(1,0.25,0)
 scene.add(directionalLight)
 
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
+scene.add(hemisphereLight)
+
 // const pointLight = new THREE.PointLight(0xffffff, 0.5)
 // pointLight.position.x = 2
 // pointLight.position.y = 3
