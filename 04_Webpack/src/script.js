4d401ae3af983ede2ee429064c8a916d87a0051a
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-15 10:25:35 +0300

    Update script.js
    pointLight position

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -30,7 +30,8 @@ scene.add(directionalLight)
 const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3)
 scene.add(hemisphereLight)
 
-const pointLight = new THREE.PointLight(0xff9000, 0.5)
+const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2)
+pointLight.position.set(1, -0.5, 1)
 scene.add(pointLight)
 
 // const pointLight = new THREE.PointLight(0xffffff, 0.5)
