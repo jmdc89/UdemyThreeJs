6df85db74a0a38109742f083aa256d92cf0d01aa
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-17 10:35:48 +0300

    Update script.js
    PointLight

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -20,12 +20,12 @@ const scene = new THREE.Scene()
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.4)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.4)
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3)
 directionalLight.position.set(2, 2, - 1)
 directionalLight.castShadow = true
 directionalLight.shadow.mapSize.width = 1024
@@ -48,7 +48,7 @@ scene.add(directionalLightCameraHelper)
 directionalLightCameraHelper.visible = false
 
 // Spot light
-const spotLight = new THREE.SpotLight(0xffffff, 0.4, 10, Math.PI * 0.3)
+const spotLight = new THREE.SpotLight(0xffffff, 0.3, 10, Math.PI * 0.3)
 
 spotLight.castShadow = true
 
@@ -64,6 +64,17 @@ spotLight.shadow.camera.far = 6
 const spotLightCameraHelper = new THREE.CameraHelper(spotLight.shadow.camera)
 scene.add(spotLightCameraHelper)
 spotLightCameraHelper.visible = false
+
+// Point light
+const pointLight = new THREE.PointLight(0xffffff, 0.3)
+
+pointLight.castShadow = true
+
+pointLight.position.set(- 1, 1, 0)
+scene.add(pointLight)
+
+const pointLightCameraHelper = new THREE.CameraHelper(pointLight.shadow.camera)
+scene.add(pointLightCameraHelper)
 
 /**
  * Materials
