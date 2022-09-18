8c1a1afbde7a34d5b62d10a6888be29ac9c504e8
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-18 15:21:33 +0300

    Update script.js
    optimization

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -262,6 +262,7 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setClearColor('#262837')
 renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 // Shadows
 moonLight.castShadow = true
@@ -277,6 +278,34 @@ bush3.castShadow = true
 bush4.castShadow = true
 
 floor.receiveShadow = true
+
+moonLight.shadow.mapSize.width = 256
+moonLight.shadow.mapSize.height = 256
+moonLight.shadow.camera.far = 15
+
+// ...
+
+doorLight.shadow.mapSize.width = 256
+doorLight.shadow.mapSize.height = 256
+doorLight.shadow.camera.far = 7
+
+// ...
+
+ghost1.shadow.mapSize.width = 256
+ghost1.shadow.mapSize.height = 256
+ghost1.shadow.camera.far = 7
+
+// ...
+
+ghost2.shadow.mapSize.width = 256
+ghost2.shadow.mapSize.height = 256
+ghost2.shadow.camera.far = 7
+
+// ...
+
+ghost3.shadow.mapSize.width = 256
+ghost3.shadow.mapSize.height = 256
+ghost3.shadow.camera.far = 7
 
 /**
  * Animate
