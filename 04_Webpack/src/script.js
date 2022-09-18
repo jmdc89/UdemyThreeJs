0d560721abd97bdfa9a5355f7da882637cd9332c
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-18 15:10:35 +0300

    Update script.js
    ghost walk

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -173,6 +173,18 @@ for(let i = 0; i < 50; i++)
 }
 
 /**
+ * Ghosts
+ */
+ const ghost1 = new THREE.PointLight('#ff00ff', 2, 3)
+ scene.add(ghost1)
+ 
+ const ghost2 = new THREE.PointLight('#00ffff', 2, 3)
+ scene.add(ghost2)
+ 
+ const ghost3 = new THREE.PointLight('#ffff00', 2, 3)
+ scene.add(ghost3)
+
+/**
  * Lights
  */
 // Ambient light
@@ -255,6 +267,22 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    // Ghosts
+    const ghost1Angle = elapsedTime * 0.5
+    ghost1.position.x = Math.cos(ghost1Angle) * 4
+    ghost1.position.z = Math.sin(ghost1Angle) * 4
+    ghost1.position.y = Math.sin(elapsedTime * 3)
+
+    const ghost2Angle = - elapsedTime * 0.32
+    ghost2.position.x = Math.cos(ghost2Angle) * 5
+    ghost2.position.z = Math.sin(ghost2Angle) * 5
+    ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+
+    const ghost3Angle = - elapsedTime * 0.18
+    ghost3.position.x = Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32))
+    ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5))
+    ghost3.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+    
     // Update controls
     controls.update()
 
