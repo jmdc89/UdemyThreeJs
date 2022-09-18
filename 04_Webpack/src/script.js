7a5ec8ede56884ab3d1bb8bd37e030a4a4fdf047
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-18 13:01:27 +0300

    Update script.js
    door

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -43,6 +43,15 @@ roof.rotation.y = Math.PI * 0.25
 roof.position.y = 2.5 + 0.5
 house.add(roof)
 
+// Door
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2, 2),
+    new THREE.MeshStandardMaterial({ color: '#aa7b7b'})
+)
+door.position.y = 1
+door.position.z = 2 + 0.01
+house.add(door)
+
 // Floor
 const floor = new THREE.Mesh(
     new THREE.PlaneGeometry(20, 20),
