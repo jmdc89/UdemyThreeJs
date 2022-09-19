d6e0cb2a840d66a8cce85932ad375f903b9e3895
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-19 16:13:40 +0300

    Update script.js
    color

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -29,33 +29,40 @@ const particleTexture = textureLoader.load('/textures/particles/2.png')
 const particlesMaterial = new THREE.PointsMaterial()
 particlesMaterial.size = 0.5
 particlesMaterial.sizeAttenuation = true
-particlesMaterial.color = new THREE.Color('#ff88cc')
+// particlesMaterial.color = new THREE.Color('#ff88cc')
 // particlesMaterial.map = particleTexture
 particlesMaterial.transparent = true
 particlesMaterial.alphaMap = particleTexture
 // particlesMaterial.alphaTest = 0.001
-particlesMaterial.depthTest = false
+// particlesMaterial.depthTest = false
+particlesMaterial.depthWrite = false
+particlesMaterial.blending = THREE.AdditiveBlending
+particlesMaterial.vertexColors = true
 
 // Geometry
 
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(),
+//     new THREE.MeshBasicMaterial()
+// )
+// scene.add(cube)
 
 
 const particlesGeometry = new THREE.BufferGeometry()
 const count = 5000
 
 const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+const colors = new Float32Array(count * 3)
 
 for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
 {
     positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+    colors[i] = Math.random()
 }
 
 particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
 // Points
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 scene.add(particles)
