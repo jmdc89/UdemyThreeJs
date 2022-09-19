c83150a731b65fd17a6487a3377110b2440e36c4
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-19 15:57:18 +0300

    Update script.js
    particles geometry

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -26,8 +26,24 @@ const textureLoader = new THREE.TextureLoader()
 //     sizeAttenuation: true
 // })
 const particlesMaterial = new THREE.PointsMaterial()
-particlesMaterial.size = 0.02
+particlesMaterial.size = 0.1
 particlesMaterial.sizeAttenuation = true
+
+// Geometry
+const particlesGeometry = new THREE.BufferGeometry()
+const count = 500
+
+const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
+
+for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
+{
+    positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
+}
+
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
+// Points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+scene.add(particles)
 
 /**
  * Sizes
