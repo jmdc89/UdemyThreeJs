031aceb1c0d32932c73b153b9fae333f4765041e
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-19 16:00:01 +0300

    Update script.js
    particles color

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -28,10 +28,11 @@ const textureLoader = new THREE.TextureLoader()
 const particlesMaterial = new THREE.PointsMaterial()
 particlesMaterial.size = 0.1
 particlesMaterial.sizeAttenuation = true
+particlesMaterial.color = new THREE.Color('#ff88cc')
 
 // Geometry
 const particlesGeometry = new THREE.BufferGeometry()
-const count = 500
+const count = 5000
 
 const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
 
