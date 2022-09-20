933222f69324b12c5e0940cacbcb6d81f066e23d
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-20 08:05:08 +0300

    galaxies
    starter pack

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -16,56 +16,13 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
+ * Test cube
  */
-const textureLoader = new THREE.TextureLoader()
-const particleTexture = textureLoader.load('/textures/particles/2.png')
-
-// Material
-// const particlesMaterial = new THREE.PointsMaterial({
-//     size: 0.02,
-//     sizeAttenuation: true
-// })
-const particlesMaterial = new THREE.PointsMaterial()
-particlesMaterial.size = 0.5
-particlesMaterial.sizeAttenuation = true
-// particlesMaterial.color = new THREE.Color('#ff88cc')
-// particlesMaterial.map = particleTexture
-particlesMaterial.transparent = true
-particlesMaterial.alphaMap = particleTexture
-// particlesMaterial.alphaTest = 0.001
-// particlesMaterial.depthTest = false
-particlesMaterial.depthWrite = false
-particlesMaterial.blending = THREE.AdditiveBlending
-particlesMaterial.vertexColors = true
-
-// Geometry
-
-// const cube = new THREE.Mesh(
-//     new THREE.BoxGeometry(),
-//     new THREE.MeshBasicMaterial()
-// )
-// scene.add(cube)
-
-
-const particlesGeometry = new THREE.BufferGeometry()
-const count = 5000
-
-const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
-const colors = new Float32Array(count * 3)
-
-for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
-{
-    positions[i] = (Math.random() - 0.5) * 10 // Math.random() - 0.5 to have a random value between -0.5 and +0.5
-    colors[i] = Math.random()
-}
-
-particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
-particlesGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-
-// Points
-const particles = new THREE.Points(particlesGeometry, particlesMaterial)
-scene.add(particles)
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial()
+)
+scene.add(cube)
 
 /**
  * Sizes
@@ -95,6 +52,8 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 3
+camera.position.y = 3
 camera.position.z = 3
 scene.add(camera)
 
@@ -119,19 +78,6 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Update particles
-    // particles.rotation.y = elapsedTime * 0.2
-
-    for(let i = 0; i < count; i++)
-    {
-        let i3 = i * 3
-
-        const x = particlesGeometry.attributes.position.array[i3]
-        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x)
-    }
-    particlesGeometry.attributes.position.needsUpdate = true
-
 
     // Update controls
     controls.update()
