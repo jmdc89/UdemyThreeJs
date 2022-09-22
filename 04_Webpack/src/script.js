75566d12d99005e375649885589c909c016150d2
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-22 10:04:41 +0300

    Update script.js
    mouse enter leave

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -102,6 +102,8 @@ controls.enableDamping = true
      console.log(mouse)
  })
 
+ let currentIntersect = null
+
 /**
  * Renderer
  */
@@ -147,23 +149,28 @@ const tick = () =>
     // }
 
     raycaster.setFromCamera(mouse, camera)
-    
     const objectsToTest = [object1, object2, object3]
     const intersects = raycaster.intersectObjects(objectsToTest)
     
-    for(const intersect of intersects)
+    if(intersects.length)
     {
-        intersect.object.material.color.set('#0000ff')
-    }
-
-    for(const object of objectsToTest)
-    {
-        if(!intersects.find(intersect => intersect.object === object))
+        if(!currentIntersect)
         {
-            object.material.color.set('#ff0000')
+            console.log('mouse enter')
         }
-    }
 
+        currentIntersect = intersects[0]
+    }
+    else
+    {
+        if(currentIntersect)
+        {
+            console.log('mouse leave')
+        }
+        
+        currentIntersect = null
+    }
+    
     // Update controls
     controls.update()
 
