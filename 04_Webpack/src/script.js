1cb6c090600560f140aeebb888f750c50929fd36
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-22 09:58:33 +0300

    Update script.js
    mouse event

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -90,6 +90,19 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 /**
+ * Mouse
+ */
+ const mouse = new THREE.Vector2()
+
+ window.addEventListener('mousemove', (event) =>
+ {
+     mouse.x = event.clientX / sizes.width * 2 - 1
+     mouse.y = - (event.clientY / sizes.height) * 2 + 1
+ 
+     console.log(mouse)
+ })
+
+/**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
@@ -112,26 +125,26 @@ const tick = () =>
     object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
     object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
 
-    // Cast a ray
-    const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-    const rayDirection = new THREE.Vector3(1, 0, 0)
-    rayDirection.normalize()
+    // // Cast a ray
+    // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
+    // const rayDirection = new THREE.Vector3(1, 0, 0)
+    // rayDirection.normalize()
     
-    raycaster.set(rayOrigin, rayDirection)
+    // raycaster.set(rayOrigin, rayDirection)
     
-    const objectsToTest = [object1, object2, object3]
-    const intersects = raycaster.intersectObjects(objectsToTest)
-    console.log(intersects)
+    // const objectsToTest = [object1, object2, object3]
+    // const intersects = raycaster.intersectObjects(objectsToTest)
+    // console.log(intersects)
 
-    for(const object of objectsToTest)
-    {
-        object.material.color.set('#ff0000')
-    }
+    // for(const object of objectsToTest)
+    // {
+    //     object.material.color.set('#ff0000')
+    // }
 
-    for(const intersect of intersects)
-    {
-        intersect.object.material.color.set('#0000ff')
-    }
+    // for(const intersect of intersects)
+    // {
+    //     intersect.object.material.color.set('#0000ff')
+    // }
 
     // Update controls
     controls.update()
