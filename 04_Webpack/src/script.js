bc9af7336fb68d4c58aa32e88be656f265e8dc6c
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-23 12:03:46 +0300

    scroll animation
    starter pack

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -1,14 +1,22 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+
+/**
+ * Debug
+ */
+const gui = new dat.GUI()
+
+const parameters = {
+    materialColor: '#ffeded'
+}
+
+gui
+    .addColor(parameters, 'materialColor')
 
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI()
-
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -16,43 +24,13 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Objects
+ * Test cube
  */
-const object1 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
+const cube = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshBasicMaterial({ color: '#ff0000' })
 )
-object1.position.x = - 2
-
-const object2 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-
-const object3 = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 16, 16),
-    new THREE.MeshBasicMaterial({ color: '#ff0000' })
-)
-object3.position.x = 2
-
-scene.add(object1, object2, object3)
-
-/**
- * Raycaster
- */
- const raycaster = new THREE.Raycaster()
-
-//  const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-// const rayDirection = new THREE.Vector3(10, 0, 0)
-// rayDirection.normalize()
-
-// raycaster.set(rayOrigin, rayDirection)
-
-// const intersect = raycaster.intersectObject(object2)
-// console.log(intersect)
-
-// const intersects = raycaster.intersectObjects([object1, object2, object3])
-// console.log(intersects)
+scene.add(cube)
 
 /**
  * Sizes
@@ -81,49 +59,9 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
 scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Mouse
- */
- const mouse = new THREE.Vector2()
-
- window.addEventListener('mousemove', (event) =>
- {
-     mouse.x = event.clientX / sizes.width * 2 - 1
-     mouse.y = - (event.clientY / sizes.height) * 2 + 1
- 
-     console.log(mouse)
- })
-
- let currentIntersect = null
-
- window.addEventListener('click', () =>
-{
-    if(currentIntersect)
-    {
-        switch(currentIntersect.object)
-        {
-            case object1:
-                console.log('click on object 1')
-                break
-
-            case object2:
-                console.log('click on object 2')
-                break
-
-            case object3:
-                console.log('click on object 3')
-                break
-        }
-    }
-})
 
 /**
  * Renderer
@@ -142,58 +80,6 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
-
-    // Animate objects
-    object1.position.y = Math.sin(elapsedTime * 0.3) * 1.5
-    object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5
-    object3.position.y = Math.sin(elapsedTime * 1.4) * 1.5
-
-    // // Cast a ray
-    // const rayOrigin = new THREE.Vector3(- 3, 0, 0)
-    // const rayDirection = new THREE.Vector3(1, 0, 0)
-    // rayDirection.normalize()
-    
-    // raycaster.set(rayOrigin, rayDirection)
-    
-    // const objectsToTest = [object1, object2, object3]
-    // const intersects = raycaster.intersectObjects(objectsToTest)
-    // console.log(intersects)
-
-    // for(const object of objectsToTest)
-    // {
-    //     object.material.color.set('#ff0000')
-    // }
-
-    // for(const intersect of intersects)
-    // {
-    //     intersect.object.material.color.set('#0000ff')
-    // }
-
-    raycaster.setFromCamera(mouse, camera)
-    const objectsToTest = [object1, object2, object3]
-    const intersects = raycaster.intersectObjects(objectsToTest)
-    
-    if(intersects.length)
-    {
-        if(!currentIntersect)
-        {
-            console.log('mouse enter')
-        }
-
-        currentIntersect = intersects[0]
-    }
-    else
-    {
-        if(currentIntersect)
-        {
-            console.log('mouse leave')
-        }
-        
-        currentIntersect = null
-    }
-
-    // Update controls
-    controls.update()
 
     // Render
     renderer.render(scene, camera)
