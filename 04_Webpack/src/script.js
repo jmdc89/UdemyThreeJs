497614025583d58910bc6a839c5927f6128f03d7
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-24 14:08:31 +0300

    Update script.js
    mousemove event

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -117,6 +117,21 @@ scene.add(camera)
 })
 
 /**
+ * Cursor
+ */
+ const cursor = {}
+ cursor.x = 0
+ cursor.y = 0
+
+ window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+
+    console.log(cursor)
+})
+
+/**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
