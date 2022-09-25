1425460e643bbf88179ce83dd2e6d84ca3c71e6b
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-09-25 16:10:06 +0300

    Update script.js
    sphereBody

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -89,6 +89,15 @@ scene.add(directionalLight)
  const world = new CANNON.World()
  world.gravity.set(0, - 9.82, 0)
 
+ const sphereShape = new CANNON.Sphere(0.5)
+
+ const sphereBody = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape: sphereShape
+})
+
+
 /**
  * Sizes
  */
