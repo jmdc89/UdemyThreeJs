14609ad14672a199680f6693ae30eec09cc63a85
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-02 14:59:49 +0300

    Update script.js
    Gltf loader

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 /**
  * Base
@@ -29,6 +30,11 @@ const floor = new THREE.Mesh(
 floor.receiveShadow = true
 floor.rotation.x = - Math.PI * 0.5
 scene.add(floor)
+
+/**
+ * Models
+ */
+ const gltfLoader = new GLTFLoader()
 
 /**
  * Lights
