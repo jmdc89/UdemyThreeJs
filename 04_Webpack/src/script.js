e4fd629a62dffecfa26da9f19c34d3ca3c78eb75
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-02 15:21:39 +0300

    Update script.js
    children 0

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -40,18 +40,7 @@ scene.add(floor)
     '/models/Duck/glTF/Duck.gltf',
     (gltf) =>
     {
-        console.log('success')
-        console.log(gltf)
-    },
-    (progress) =>
-    {
-        console.log('progress')
-        console.log(progress)
-    },
-    (error) =>
-    {
-        console.log('error')
-        console.log(error)
+        scene.add(gltf.scene.children[0])
     }
 )
 
