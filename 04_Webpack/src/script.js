b5841325a082ab4ac9b00185dbdc1bdecacb2226
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-02 16:09:20 +0300

    Update script.js
    dracoloader import

--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 /**
  * Base
