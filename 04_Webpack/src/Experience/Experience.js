b9b5690e2c87fab14185a70c1df4f593fea948ce
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-14 11:00:13 +0300

    experience file
    start

---
+++ 04_Webpack/src/Experience/Experience.js
@@ -0,0 +1,7 @@
+export default class Experience
+{
+    constructor()
+    {
+        console.log('Here starts a great experience')
+    }
+}
--- 04_Webpack/src/script.js
+++ 04_Webpack/src/script.js
@@ -3,6 +3,10 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import Experience from './Experience/Experience.js'
+
+const experience = new Experience()
+
 
 /**
  * Loaders
