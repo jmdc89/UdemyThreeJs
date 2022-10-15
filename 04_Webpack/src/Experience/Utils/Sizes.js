3a0d89b5becc1cf024a8743239b335dde7e9bfd4
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-15 12:26:33 +0300

    Update Sizes.js
    resize event

--- 04_Webpack/src/Experience/Utils/Sizes.js
+++ 04_Webpack/src/Experience/Utils/Sizes.js
@@ -2,5 +2,17 @@ export default class Sizes
 {
     constructor()
     {
+        // Setup
+        this.width = window.innerWidth
+        this.height = window.innerHeight
+        this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+
+        // Resize event
+        window.addEventListener('resize', () =>
+        {
+            this.width = window.innerWidth
+            this.height = window.innerHeight
+            this.pixelRatio = Math.min(window.devicePixelRatio, 2)
+        })
     }
 }
