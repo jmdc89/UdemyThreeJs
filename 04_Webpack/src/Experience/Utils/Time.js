cb1bc634063e63b269600a74943c8cc1289413ae
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-16 15:58:54 +0300

    Update Time.js
    current time

--- 04_Webpack/src/Experience/Utils/Time.js
+++ 04_Webpack/src/Experience/Utils/Time.js
@@ -20,8 +20,13 @@ export default class Time extends EventEmitter
 
     tick()
     {
-        console.log('tick')
+        const currentTime = Date.now()
+        this.delta = currentTime - this.current
+        this.current = currentTime
+        this.elapsed = this.current - this.start
 
+        this.trigger('tick')
+        
         window.requestAnimationFrame(() =>
         {
             this.tick()
