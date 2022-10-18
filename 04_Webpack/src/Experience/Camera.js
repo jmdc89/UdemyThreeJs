787485abf6367c1ecff418d13c4f004a2c8af67f
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-18 11:02:36 +0300

    Update Camera.js
    perpspective camera

--- 04_Webpack/src/Experience/Camera.js
+++ 04_Webpack/src/Experience/Camera.js
@@ -1,3 +1,4 @@
+import * as THREE from 'three'
 import Experience from './Experience.js'
 
 export default class Camera
@@ -9,5 +10,13 @@ export default class Camera
         this.scene = this.experience.scene
         this.canvas = this.experience.canvas
         
+        this.setInstance()
+    }
+
+    setInstance()
+    {
+        this.instance = new THREE.PerspectiveCamera(35, this.sizes.width / this.sizes.height, 0.1, 100)
+        this.instance.position.set(6, 4, 8)
+        this.scene.add(this.instance)
     }
 }
