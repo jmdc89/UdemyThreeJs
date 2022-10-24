ed2aaa765940fea29b04eecb8cee9ceba0af4199
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-24 08:58:56 +0300

    Update Resources.js
    startLoading

--- 04_Webpack/src/Experience/Utils/Resources.js
+++ 04_Webpack/src/Experience/Utils/Resources.js
@@ -16,6 +16,7 @@ export default class Resources extends EventEmitter
         this.toLoad = this.sources.length
         this.loaded = 0
         this.setLoaders()
+        this.startLoading()
     }
 
     setLoaders()
@@ -24,5 +25,43 @@ export default class Resources extends EventEmitter
         this.loaders.gltfLoader = new GLTFLoader()
         this.loaders.textureLoader = new THREE.TextureLoader()
         this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader()
+    }
+
+    startLoading()
+    {
+        // Load each source
+        for(const source of this.sources)
+        {
+            if(source.type === 'gltfModel')
+            {
+                this.loaders.gltfLoader.load(
+                    source.path,
+                    (file) =>
+                    {
+                        console.log(source, file)
+                    }
+                )
+            }
+            else if(source.type === 'texture')
+            {
+                this.loaders.textureLoader.load(
+                    source.path,
+                    (file) =>
+                    {
+                        console.log(source, file)
+                    }
+                )
+            }
+            else if(source.type === 'cubeTexture')
+            {
+                this.loaders.cubeTextureLoader.load(
+                    source.path,
+                    (file) =>
+                    {
+                        console.log(source, file)
+                    }
+                )
+            }
+        }
     }
 }
