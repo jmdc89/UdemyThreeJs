5067a50e8e0ea8d7dc02389c2f91c87fa549c8e4
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-24 08:57:31 +0300

    Update Resources.js
    setLoaders

--- 04_Webpack/src/Experience/Utils/Resources.js
+++ 04_Webpack/src/Experience/Utils/Resources.js
@@ -15,5 +15,14 @@ export default class Resources extends EventEmitter
         this.items = {}
         this.toLoad = this.sources.length
         this.loaded = 0
+        this.setLoaders()
+    }
+
+    setLoaders()
+    {
+        this.loaders = {}
+        this.loaders.gltfLoader = new GLTFLoader()
+        this.loaders.textureLoader = new THREE.TextureLoader()
+        this.loaders.cubeTextureLoader = new THREE.CubeTextureLoader()
     }
 }
