6ba161305ccd4cce31bf7c08041cf1129d0ad44e
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-24 08:56:49 +0300

    Update Resources.js
    Loaders import

--- 04_Webpack/src/Experience/Utils/Resources.js
+++ 04_Webpack/src/Experience/Utils/Resources.js
@@ -1,4 +1,6 @@
 import EventEmitter from './EventEmitter.js'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 export default class Resources extends EventEmitter
 {
