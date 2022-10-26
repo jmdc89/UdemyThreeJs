03131728bd1cc5f05483b9e28619029f6791354a
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-26 12:52:48 +0300

    Update Environment.js
    setEnvironmentMap

--- 04_Webpack/src/Experience/World/Environment.js
+++ 04_Webpack/src/Experience/World/Environment.js
@@ -7,9 +7,11 @@ export default class Environment
     {
         this.experience = new Experience()
         this.scene = this.experience.scene
-    
+        this.resources = this.experience.resources
+
         // Setup
         this.setSunLight()
+        this.setEnvironmentMap()
     }
 
     setSunLight()
@@ -21,5 +23,15 @@ export default class Environment
         this.sunLight.shadow.normalBias = 0.05
         this.sunLight.position.set(3, 3, - 2.25)
         this.scene.add(this.sunLight)
+    }
+
+    setEnvironmentMap()
+    {
+        this.environmentMap = {}
+        this.environmentMap.intensity = 0.4
+        this.environmentMap.texture = this.resources.items.environmentMapTexture
+        this.environmentMap.texture.encoding = THREE.sRGBEncoding
+        
+        this.scene.environment = this.environmentMap.texture
     }
 }
