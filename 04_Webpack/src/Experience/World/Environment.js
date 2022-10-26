ed32c5807ec2eba26db75a990b203423bee70854
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-26 12:54:19 +0300

    Update Environment.js
    updateMaterialsMethod

--- 04_Webpack/src/Experience/World/Environment.js
+++ 04_Webpack/src/Experience/World/Environment.js
@@ -33,5 +33,19 @@ export default class Environment
         this.environmentMap.texture.encoding = THREE.sRGBEncoding
         
         this.scene.environment = this.environmentMap.texture
+    
+        this.environmentMap.updateMaterials = () =>
+        {
+            this.scene.traverse((child) =>
+            {
+                if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
+                {
+                    child.material.envMap = this.environmentMap.texture
+                    child.material.envMapIntensity = this.environmentMap.intensity
+                    child.material.needsUpdate = true
+                }
+            })
+        }
+        this.environmentMap.updateMaterials()
     }
 }
