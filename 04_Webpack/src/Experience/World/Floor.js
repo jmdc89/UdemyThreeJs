f818724cd236379abf89c070755a396b910fc1c6
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-27 07:55:04 +0300

    Update Floor.js
    setMesh

--- 04_Webpack/src/Experience/World/Floor.js
+++ 04_Webpack/src/Experience/World/Floor.js
@@ -43,4 +43,12 @@ export default class Floor
             normalMap: this.textures.normal
         })
     }
+
+    setMesh()
+    {
+        this.mesh = new THREE.Mesh(this.geometry, this.material)
+        this.mesh.rotation.x = - Math.PI * 0.5
+        this.mesh.receiveShadow = true
+        this.scene.add(this.mesh)
+    }
 }
