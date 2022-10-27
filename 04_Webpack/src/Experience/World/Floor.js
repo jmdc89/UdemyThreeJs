45559b541fb562319962c6b34e7644583e6df180
jmdc89 <68101685+jmdc89@users.noreply.github.com>
2022-10-27 07:54:19 +0300

    Update Floor.js
    setTextures

--- 04_Webpack/src/Experience/World/Floor.js
+++ 04_Webpack/src/Experience/World/Floor.js
@@ -19,4 +19,20 @@ export default class Floor
     {
         this.geometry = new THREE.CircleGeometry(5, 64)
     }
+
+    setTextures()
+    {
+        this.textures = {}
+
+        this.textures.color = this.resources.items.grassColorTexture
+        this.textures.color.encoding = THREE.sRGBEncoding
+        this.textures.color.repeat.set(1.5, 1.5)
+        this.textures.color.wrapS = THREE.RepeatWrapping
+        this.textures.color.wrapT = THREE.RepeatWrapping
+
+        this.textures.normal = this.resources.items.grassNormalTexture
+        this.textures.normal.repeat.set(1.5, 1.5)
+        this.textures.normal.wrapS = THREE.RepeatWrapping
+        this.textures.normal.wrapT = THREE.RepeatWrapping
+    }
 }
